9e6339985aaa7f08f0fe3c78e5641d32267bfda9
dingxianli <dingxianli@dds.com.cn>
2018-10-19 10:57:30 +0800

    Update build

--- python/hello.py
+++ python/hello.py
@@ -1,7 +1,8 @@
 import logging
+import numpy as np
 
 log = logging.getLogger(__name__)
 
-log.warning('log: Hello World!')
+log.warning('log: Hello World! numpy.dot: %d' % np.dot(2, 3))
 
-print('print: Hello World!')
+print('print: Hello World! numpy.dot: %d' % np.dot(2, 3))

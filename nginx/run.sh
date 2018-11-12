118e9e61483937236e3261cc43c227fdb7832337
dingxl <dingxl@dds.com.cn>
2018-11-12 10:59:34 +0800

    Update ngix.conf

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,7 +3,7 @@ clear
 
 ./stop.sh
 
-docker run -it --name nginx -p 80:80 -v $PWD:/usr/share/nginx/html:ro -d nginx:stable
+docker run -it --name nginx -p 80:80 -v $PWD/web:/usr/share/nginx/html:ro -d nginx:stable
 
 docker port nginx
 docker ps

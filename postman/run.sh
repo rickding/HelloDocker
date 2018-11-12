c606ed53ee338f61d148ab0ccdcd0f81d7c526d8
dingxl <dingxl@dds.com.cn>
2018-11-12 10:09:16 +0800

    Update postman

--- postman/run.sh
+++ postman/run.sh
@@ -6,10 +6,10 @@ clear
 docker run -t postman/newman --url="https://www.getpostman.com/collections/8a0c9bc08f062d12dcda"
 
 # set collection volume
-docker run -v $PWD/collections:/etc/postman -t postman/newman \
-    https://www.getpostman.com/collections/df0a40ef41d570c48154 \
-    --environment="HTTPBinNewmanTest.json.postman_environment" \
-    --reporters junit --reporter-junit-export="newman-report.xml"
+#docker run -v $PWD/collections:/etc/postman -t postman/newman \
+#    https://www.getpostman.com/collections/df0a40ef41d570c48154 \
+#    --environment="HTTPBinNewmanTest.json.postman_environment" \
+#    --reporters junit --reporter-junit-export="newman-report.xml"
 
 docker port postman
 docker ps

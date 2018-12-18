392a8b3c7c0b51035f9576645a1acc66de3eed62
dingxl <dingxl@dds.com.cn>
2018-12-18 11:24:46 +0800

    setup mysql

--- jira/initdb/init.sql
+++
@@ -1,8 +0,0 @@
-drop database if exists jira;
-
-create database jira default character set utf8 collate utf8_bin;
-CREATE USER `jira`@`%` IDENTIFIED BY 'jira';GRANT ALL ON *.* TO `jira`@`%` WITH GRANT OPTION;
-alter user 'jira'@'%' identified with mysql_native_password by 'jira';
-
-set global transaction isolation level read committed;
-set session transaction isolation level read committed;

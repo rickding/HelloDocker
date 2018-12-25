23059ac35024693ebb679650ba2685400bafee2b
dingxl <dingxl>
2018-12-25 22:10:28 +0800

    push img

---
+++ weblogic12/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ./build.sh
+# ./login.sh
+
+# img:tag repo:tag namespace server
+set weblogic12:latest weblogic12:latest hellodock registry.cn-shanghai.aliyuncs.com
+
+docker tag $1 $4/$3/$2
+docker push $4/$3/$2
+
+# docker rmi $4/$3/$2
+# docker pull $4/$3/$2

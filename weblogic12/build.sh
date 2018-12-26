b78e69202b7d313b973c42cf12417822ece6d8b5
dingxl <dingxl@dds.com.cn>
2018-12-26 15:18:28 +0800

    test springboot war with weblogic

--- weblogic12/build.sh
+++ weblogic12/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+cp ../../HelloJava/HelloEar/ear.ear ./webapps
+
+cp ../../HelloJava/HelloWar/target/war-0.0.1-SNAPSHOT.war ./webapps
+cp ../../HelloJava/HelloWebWar/target/web_war-0.0.1-SNAPSHOT.war ./webapps
+
+cp ../../nx/ams-web/target/ams.war ./webapps
+
 docker-compose build

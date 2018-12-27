de4bd6cbd028836b507d43e87672415947bc5733
dingxl <dingxl@dds.com.cn>
2018-12-27 11:56:39 +0800

    update

--- weblogic12/build.sh
+++ weblogic12/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# war and springboot
 cd ../../HelloJava/HelloWar/
 ./package.sh
 
@@ -11,7 +12,13 @@ cd ../../HelloDocker/weblogic12
 cp ../../HelloJava/HelloWar/target/war-0.0.1-SNAPSHOT.war ./webapps
 cp ../../HelloJava/HelloWebWar/target/web_war-0.0.1-SNAPSHOT.war ./webapps
 
+# ear
 cp ../../HelloJava/HelloEar/ear.ear ./webapps
+
+# ams
+cd ../../nx/ams/
+./package.sh
+cd ../../HelloDocker/weblogic12
 cp ../../nx/ams-web/target/ams.war ./webapps
 
 docker-compose build

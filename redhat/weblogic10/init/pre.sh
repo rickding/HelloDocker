c264e2f0a626c3a3b7c9cdc87a5e42c6ed38923c
dingxl <dingxl>
2019-01-03 22:58:09 +0800

    wls10

--- redhat/weblogic10/init/pre.sh
+++ redhat/weblogic10/init/pre.sh
@@ -4,11 +4,11 @@
 java -version
 echo "JAVA_HOME: " $JAVA_HOME
 
+# user
+groupadd weblogic
+useradd -g weblogic weblogic
+
 # installer
 mv wls1036_generic.jar /home/weblogic
 mv wls.sh /home/weblogic
 mv boot.properties /home/weblogic
-
-# user
-groupadd weblogic
-useradd -g weblogic weblogic

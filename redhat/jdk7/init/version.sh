336b96652ea2f67126d1830093103f4bf1212a1a
dingxl <dingxl@dds.com.cn>
2019-01-03 10:37:01 +0800

    update fir

---
+++ redhat/jdk7/init/version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# os
+cat /etc/redhat-release
+# cat /etc/issue
+# lsb_release -a
+# uname -a
+# cat /proc/version
+
+# 64bit?
+arch
+
+# environment
+echo "PWD: " $PWD
+echo "PATH: " $PATH
+
+# java
+java -version
+
+# /usr/lib/jvm/java-1.8.0
+echo "JAVA_HOME: " $JAVA_HOME
+
+# yum
+yum repolist all | grep enabled
+cd /etc/yum.repos.d/ && ls -al ./
+
+# installed yum rpm
+rpm -qa | grep yum

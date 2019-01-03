bceaa0269a06c7744f04ebb6439e3b3cf36cfe70
dingxl <dingxl>
2019-01-03 23:29:12 +0800

    update

--- etcd/pull.sh
+++ etcd/pull.sh
@@ -3,4 +3,4 @@
 # docker pull gcr.io/etcd-development/etcd:v3.3.10
 docker pull quay.io/coreos/etcd:v3.3.10
 
-docker pull registry.cn-shanghai.aliyuncs.com/hellodock/etcd:3.3.10
+# docker pull registry.cn-shanghai.aliyuncs.com/hellodock/etcd:3.3.10
--- redhat/weblogic10/pull.sh
+++ redhat/weblogic10/pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker pull registry.cn-shanghai.aliyuncs.com/hellodock/redhat:6.5.11
+# docker pull registry.access.redhat.com/rhel6/rhel:6.5-11
+# docker pull wjp719/redhat6.5:latest
+# docker pull registry.cn-shanghai.aliyuncs.com/hellodock/redhat:6.5.11
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/redhat:6.5.11.jdk7
 # docker pull registry.cn-shanghai.aliyuncs.com/hellodock/redhat:6.5.11.wls10.jdk7

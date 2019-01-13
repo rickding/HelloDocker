bdb55316a351e1d073e9144b5feda1a34d5f0bcf
dingxl <dingxl>
2019-01-13 23:21:40 +0800

    update

--- jira/deploy/exec.sh
+++ jira/deploy/exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it jira_jira_1 bash
+docker exec -it deploy_jira_1 bash
--- jira/deploy/up.sh
+++ jira/deploy/up.sh
@@ -8,4 +8,4 @@ docker-compose up -d
 docker-compose ps
 
 # docker-compose logs -f
-# docker exec -it jira_jira_1 bash
+# docker exec -it deploy_jira_1 bash

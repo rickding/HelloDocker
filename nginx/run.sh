21d73a558ee7cc86e17210b730802166ee11c48a
dingxl <dingxl>
2020-02-04 10:01:00 +0800

    Remove the duplicated COPY

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,7 +2,9 @@
 
 ./stop.sh
 
-docker run -it --name nginx -p 80:80 -v $PWD/web:/usr/share/nginx/html:ro -d nginx:stable
+# docker build -t nginx:latest .
+# docker run -it --name nginx -p 80:80 -v $PWD/web:/usr/share/nginx/html:ro -d nginx:latest
+docker run -it --name nginx -p 80:80 -d nginx_local:latest
 
 docker port nginx
 docker ps

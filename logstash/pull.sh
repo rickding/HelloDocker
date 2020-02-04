639aa7e4b5b5e4912ac73dd50b96fde135e739df
dingxl <dingxl>
2020-02-04 15:38:06 +0800

    use logstash 7.5.2

--- logstash/pull.sh
+++ logstash/pull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker pull logstash:latest
+docker.elastic.co/logstash/logstash:7.5.2
+# docker pull logstash:latest
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:latest

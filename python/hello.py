e34ad70533415fb112b52f35830885299ba3a431
dingxl <dingxl>
2020-03-11 20:41:51 +0800

    update python

--- python/hello.py
+++ python/hello.py
@@ -4,17 +4,22 @@ import logging
 
 import numpy as np
 
-log = logging.getLogger(__name__)
-
+# 设置log
 DATEFMT = "[%Y-%m-%d %H:%M:%S]"
 FORMAT = "%(asctime)s %(thread)d %(message)s"
 logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt=DATEFMT)
 
+log = logging.getLogger(__name__)
+
+# hello world
 msg = "log: Hello World from python app! numpy.dot: %d" % np.dot(2, 3)
 log.info(msg)
 print(msg)
 
-# read csv: /tcdata/num_list.csv
+# Docker入门第一课，阿里云天池Docker练习场
+# https://tianchi.aliyun.com/competition/entrance/231759/introduction
+
+# 读取csv: /tcdata/num_list.csv，读取试题指定目录失败时，尝试当前目录方便测试
 f = None
 try:
     f = open("/tcdata/num_list.csv")
@@ -22,6 +27,7 @@ except FileNotFoundError as e:
     log.info(e)
     f = open("./num_list.csv")
 
+# 读取数字
 num_list = []
 f_csv = csv.reader(f)
 for row in f_csv:
@@ -30,15 +36,17 @@ for row in f_csv:
 
 f.close()
 
+# 求和
 log.info(num_list)
 num_sum = sum(num_list)
 
-# remove duplicated
+# 去重，注意试题不需要
 # num_list = list(set(num_list))
 
-# sort array
+# 排序
 num_list.sort(reverse=True)
 
+# 拼装数据结果
 result = {
     "Q1": "Hello world",
     "Q2": num_sum,
@@ -46,6 +54,6 @@ result = {
 }
 log.info(result)
 
-# save file
+# 保存结果到文件中
 with open("result.json", "w") as f:
     json.dump(result, f)
